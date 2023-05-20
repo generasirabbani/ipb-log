3a61a4e7020dd221c5881ae9b7dac9534143124a
harizkrisha <harizkrisha@apps.ipb.ac.id>
2023-05-20 21:24:37 +0700

    Working homepage

--- src/config/redux/action/index.js
+++ src/config/redux/action/index.js
@@ -6,6 +6,20 @@ export const actionUserName = () => (dispatch) => {
   }, 2000)
 }
 
+export const logoutUserAPI = () => (dispatch) => {
+  return new Promise((resolve, reject) => {
+    firebase.auth().signOut()
+      .then(() => {
+        dispatch({ type: 'LOGOUT_USER' });
+        resolve(true);
+      })
+      .catch((error) => {
+        console.log('Logout error:', error);
+        reject(false);
+      });
+  });
+};
+
 export const registerUserAPI = (data) => (dispatch) => {
   return new Promise((resolve, reject) => {
     dispatch({type: 'CHANGE_LOADING', value: true})
@@ -62,23 +76,50 @@ export const addDataToAPI = (data) => (dispatch) => {
 }
 
 export const getDataFromAPI = (userId) => (dispatch) => {
-  const urlNotes = database.ref('posts/' + userId);
+  const urlNotes = userId ? database.ref('posts/' + userId) : database.ref('posts');
   return new Promise((resolve, reject) => {
     urlNotes.on('value', (snapshot) => {
       console.log('get Data: ', snapshot.val());
       const data = [];
-      Object.keys(snapshot.val()).map(key => {
-        data.push({
-          id: key,
-          data: snapshot.val()[key]
-        })
+      if (snapshot.val()) {
+        Object.keys(snapshot.val()).map((key) => {
+          data.push({
+            id: key,
+            data: snapshot.val()[key],
+          });
+        });
+      }
+      if (userId) {
+        dispatch({ type: 'SET_NOTES', value: data });
+      } else {
+        dispatch({ type: 'SET_OTHER_USER_NOTES', value: data });
+      }
+      resolve(snapshot.val());
+    });
+  });
+};
+
+export const getAllNotesFromAPI = () => (dispatch) => {
+  const urlNotes = database.ref('posts');
+  return new Promise((resolve, reject) => {
+    urlNotes.on('value', (snapshot) => {
+      console.log('get All Notes: ', snapshot.val());
+      const data = [];
+      snapshot.forEach((userSnapshot) => {
+        Object.keys(userSnapshot.val()).map((key) => {
+          data.push({
+            id: key,
+            data: userSnapshot.val()[key],
+          });
+        });
       });
 
-      dispatch({type: 'SET_NOTES', value: data})
-      resolve(snapshot.val())
+      dispatch({ type: 'SET_NOTES', value: data });
+      resolve(snapshot.val());
     });
-  })
-}
+  });
+};
+
 
 export const updateDataAPI = (data) => (dispatch) => {
   const urlNotes = database.ref(`posts/${data.userId}/${data.noteId}`);
@@ -110,33 +151,6 @@ export const resetPasswordByEmail = (data) => (dispatch) => {
     firebase.auth().sendPasswordResetEmail(data.email)
       .then(res => {
         dispatch({type: 'CHANGE_LOADING', value: false})
-        resolve(true)
-      })
-      .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(errorCode, errorMessage)
-        dispatch({type: 'CHANGE_LOADING', value: false})
-        reject(false)
-      })
-  })
-}
-
-export const logoutUserAPI = () => (dispatch) => {
-  return new Promise((resolve, reject) => {
-    dispatch({type: 'CHANGE_LOADING', value: true})
-    firebase.auth().signOut()
-      .then(() => {
-      //   console.log('success: ', res);
-        const dataUser = {
-          email: '',
-          uid: '',
-          emailVerified: '',
-          refreshToken: ''
-        }
-        dispatch({type: 'CHANGE_LOADING', value: false})
-        dispatch({type: 'CHANGE_ISLOGIN', value: false})
-        dispatch({type: 'CHANGE_USER', value: dataUser})
         resolve(true)
       })
       .catch((error) => {
